e903def1a4c129823a35f6e2eafc4ddbd5d854d4
Fesuy <fesuybiz@gmail.com>
2023-10-26 17:56:24 +0700

    Change structure

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import Logger from './logger.js';
 // Creating new instance logger in every file make me boring
 let logger = void 0;
 
-export default {
-  /**
-   * Create logger instance once
-   * @param filepath
-   */
-  createLogger: filepath => {
-    if (!logger) {
-      logger = new Logger(filepath);
-    }
-  },
-  /**
-   * Retrieve logger instance
-   * @returns Logger
-   */
-  getLogger: () => {
-    return logger;
+/**
+ * Create logger instance once
+ * @param filepath
+ */
+export default function createLogger(filepath) {
+  if (!logger) {
+    logger = new Logger(filepath);
   }
+}
+
+/**
+ * Retrieve logger instance
+ * @returns Logger
+ */
+export default function getLogger() {
+  return logger;
 }
