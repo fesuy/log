188d959e7b1dcddb0e1b25dfd582a1b88374acea
Fesuy <fesuybiz@gmail.com>
2023-10-29 20:09:09 +0700

    Add max file in log directory

--- src/file-writer.js
+++ src/file-writer.js
@@ -18,6 +18,8 @@ class FileWriter {
     this.DATE_SEPARATOR   = '-'; // Separator
     this.fs               = void 0; // Initialize fs
     this.currentPath      = void 0;
+
+    this.MAX_LOG_FILE     = 7;
   }
 
   /**
@@ -52,9 +54,47 @@ class FileWriter {
     const changed     = newLogPath !== this.currentPath;
 
     // Log path different
-    if (changed) this.currentPath = newLogPath;
+    // Remove other log file when reached MAX_LOG_FILE
+    if (changed) {
+      this.currentPath = newLogPath;
+      this.removeUnusedLog_();
+    }
 
     return changed;
+  }
+
+  /**
+   * Remove Unused log files
+   */
+  removeUnusedLog_() {
+    const that = this;
+
+    fs.readdir(that.logDir, (err, files) => {
+      if (err) {
+        throw err;
+      }
+
+      // 1. Get time modified of file
+      // 2. Filter only `.log` extension
+      // 3. Sort file by modified
+      const logFiles = files.map(filename => ({
+          name: filename,
+          time: fs.statSync(path.join(that.logDir, filename)).mtime.getTime()
+        })) // 1
+        .filter(file => path.extname(file.name) == '.log') // 2
+        .sort((a, b) => a.time - b.time); // 3
+
+      if (logFiles.length > that.MAX_LOG_FILE) {
+        for (let i=0;i<(logFiles.length - that.MAX_LOG_FILE);i++) {
+          const logFile = path.join(that.logDir, logFiles[i].name);
+
+          // Untrusted
+          if (logFile !== that.currentPath) {
+            fs.unlinkSync(logFile);
+          }
+        }
+      }
+    });
   }
 
   /**
